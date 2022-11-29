8b14407e3ef2af586cf0a0c31c5a9959afe28bf7
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-11-29 10:08:43 +0530

    api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PassengerComponent } from './passenger/passenger.component';
 import { ViewUsPublicComponent } from './view-us-public/view-us-public.component';
+import { ViewUserInfoComponent } from './view-user-info/view-user-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PassengerComponent,
-    ViewUsPublicComponent
+    ViewUsPublicComponent,
+    ViewUserInfoComponent
   ],
   imports: [
     BrowserModule,
